7628ae81f2bffbd30c8b89063f5efcad1ec4d189
Allen <allenlabrague06@gmail.com>
2025-06-25 21:04:03 +0800

    feat: Add multi-report selection and export functionality to Arrival Reports

--- app/Exports/ArrivalReportsExport.php
+++ app/Exports/ArrivalReportsExport.php
@@ -9,15 +9,26 @@ use Illuminate\Support\Facades\Auth;
 
 class ArrivalReportsExport implements FromView
 {
+    protected $reportIds;
+
+    public function __construct($reportIds = null)
+    {
+        $this->reportIds = $reportIds;
+    }
+
     public function view(): View
     {
         $assignedVesselIds = Auth::user()->vessels()->pluck('vessels.id');
 
-        $reports = Voyage::with(['vessel', 'unit', 'remarks', 'master_info', 'noon_report', 'rob_fuel_reports'])
+        $query = Voyage::with(['vessel', 'unit', 'remarks', 'master_info', 'noon_report', 'rob_fuel_reports'])
             ->where('report_type', 'Arrival Report')
-            ->whereIn('vessel_id', $assignedVesselIds)
-            ->latest()
-            ->get();
+            ->whereIn('vessel_id', $assignedVesselIds);
+
+        if ($this->reportIds) {
+            $query->whereIn('id', $this->reportIds);
+        }
+
+        $reports = $query->get();
 
         return view('exports.arrival-reports', [
             'reports' => $reports,
