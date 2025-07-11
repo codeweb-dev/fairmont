c2c743cee5c0a6e2acb57a158c164734173c85dd
Allen <allenlabrague06@gmail.com>
2025-07-11 22:44:26 +0800

    refactor: Simplify table header structure and improve readability in Crew Monitoring Plan Report

--- resources/views/exports/crew-monitoring-plan-reports-by-date.blade.php
+++ resources/views/exports/crew-monitoring-plan-reports-by-date.blade.php
@@ -1,38 +1,37 @@
 <table border="1" cellspacing="0" cellpadding="4" style="border-collapse: collapse; width: 100%;">
     <thead>
         <tr>
-            @php
-                $headers = [
-                    'Vessel',
+            {{-- Board Crew headers --}}
+            <th style="width: 150px;">No</th>
+            <th style="width: 150px;">Crew Surname</th>
+            <th style="width: 150px;">Crew First Name</th>
+            <th style="width: 150px;">Rank</th>
+            <th style="width: 150px;">Crew Nationality</th>
+            <th style="width: 150px;">Joining Date</th>
+            <th style="width: 150px;">Days to Contract Completion</th>
+            <th style="width: 150px;">Current Date</th>
+            <th style="width: 150px;">Date to Contract Completion</th>
+            <th style="width: 150px;">No of Months On Board</th>
 
-                    // Board Crew
-                    'No', 'Crew Surname', 'Crew First Name', 'Rank', 'Crew Nationality',
-                    'Joining Date', 'Days to contract completion', 'Current Date',
-                    'Date to Contract Completion', 'No of Months On Board',
+            {{-- Crew Change headers --}}
+            <th style="width: 150px;">Port</th>
+            <th style="width: 150px;">Country</th>
+            <th style="width: 150px;">Date of Joiners Boarding</th>
+            <th style="width: 150px;">Date of Off-signers Sign Off</th>
+            <th style="width: 150px;">Joiners Ranks</th>
+            <th style="width: 150px;">Off-signers Ranks</th>
+            <th style="width: 150px;">Total Crew Change</th>
+            <th style="width: 150px;">Reason for Change</th>
+            <th style="width: 150px;">Remarks</th>
 
-                    // Crew Change
-                    'Port', 'Country', 'Date of Joiners Boarding',
-                    'Date of Off-signers Sign Off', 'Joiners Ranks', 'Off-signers Ranks',
-                    'Total Crew Change', 'Reason for Change', 'Remarks',
-
-                    // Report Remarks & Master Info
-                    'Remarks', 'Master Information',
-                ];
-            @endphp
-
-            @foreach ($headers as $header)
-                <th style="width: 250px;">{{ $header }}</th>
-            @endforeach
+            {{-- General --}}
+            <th style="width: 150px;">Remarks</th>
+            <th style="width: 150px;">Master Information</th>
         </tr>
     </thead>
+
     <tbody>
         @foreach ($reports as $report)
-            @if (!$loop->first)
-                <tr>
-                    <td colspan="11" style="height: 15px;"></td> {{-- Spacer row --}}
-                </tr>
-            @endif
-
             @php
                 $crewList = $report->board_crew ?? [];
                 $changeList = $report->crew_change ?? [];
@@ -44,36 +43,34 @@
                     $crew = $crewList[$i] ?? null;
                     $change = $changeList[$i] ?? null;
                 @endphp
+
                 <tr>
-                    {{-- General Info --}}
-                    <td>{{ $report->vessel->name ?? 'N/A' }}</td>
+                    {{-- Board Crew columns --}}
+                    <td style="width: 150px;">{{ $crew->no ?? '' }}</td>
+                    <td style="width: 150px;">{{ $crew->crew_surname ?? '' }}</td>
+                    <td style="width: 150px;">{{ $crew->crew_first_name ?? '' }}</td>
+                    <td style="width: 150px;">{{ $crew->rank ?? '' }}</td>
+                    <td style="width: 150px;">{{ $crew->crew_nationality ?? '' }}</td>
+                    <td style="width: 150px;">{{ $crew?->joining_date ? \Carbon\Carbon::parse($crew->joining_date)->format('M d, Y h:i A') : '' }}</td>
+                    <td style="width: 150px;">{{ $crew->days_contract_completion ?? '' }}</td>
+                    <td style="width: 150px;">{{ $crew?->current_date ? \Carbon\Carbon::parse($crew->current_date)->format('M d, Y h:i A') : '' }}</td>
+                    <td style="width: 150px;">{{ $crew?->contract_completion ? \Carbon\Carbon::parse($crew->contract_completion)->format('M d, Y h:i A') : '' }}</td>
+                    <td style="width: 150px;">{{ $crew->months_on_board ?? '' }}</td>
 
-                    {{-- Board Crew Info --}}
-                    <td>{{ $crew?->no ?? '' }}</td>
-                    <td>{{ $crew?->crew_surname ?? '' }}</td>
-                    <td>{{ $crew?->crew_first_name ?? '' }}</td>
-                    <td>{{ $crew?->rank ?? '' }}</td>
-                    <td>{{ $crew?->crew_nationality ?? '' }}</td>
-                    <td>{{ $crew?->joining_date ? \Carbon\Carbon::parse($crew->joining_date)->format('M d, Y h:i A') : '' }}</td>
-                    <td>{{ $crew?->contract_completion ? \Carbon\Carbon::parse($crew->contract_completion)->format('M d, Y h:i A') : '' }}</td>
-                    <td>{{ $crew?->current_date ? \Carbon\Carbon::parse($crew->current_date)->format('M d, Y h:i A') : '' }}</td>
-                    <td>{{ $crew?->days_contract_completion ?? '' }}</td>
-                    <td>{{ $crew?->months_on_board ?? '' }}</td>
+                    {{-- Crew Change columns --}}
+                    <td style="width: 150px;">{{ $change->port ?? '' }}</td>
+                    <td style="width: 150px;">{{ $change->country ?? '' }}</td>
+                    <td style="width: 150px;">{{ $change?->joiners_boarding ? \Carbon\Carbon::parse($change->joiners_boarding)->format('M d, Y h:i A') : '' }}</td>
+                    <td style="width: 150px;">{{ $change?->off_signers ? \Carbon\Carbon::parse($change->off_signers)->format('M d, Y h:i A') : '' }}</td>
+                    <td style="width: 150px;">{{ $change->joiner_ranks ?? '' }}</td>
+                    <td style="width: 150px;">{{ $change->off_signers_ranks ?? '' }}</td>
+                    <td style="width: 150px;">{{ $change->total_crew_change ?? '' }}</td>
+                    <td style="width: 150px;">{{ $change->reason_change ?? '' }}</td>
+                    <td style="width: 150px;">{{ $change->remarks ?? '' }}</td>
 
-                    {{-- Crew Change Info --}}
-                    <td>{{ $change?->port ?? '' }}</td>
-                    <td>{{ $change?->country ?? '' }}</td>
-                    <td>{{ $change?->joiners_boarding ? \Carbon\Carbon::parse($change->joiners_boarding)->format('M d, Y h:i A') : '' }}</td>
-                    <td>{{ $change?->off_signers ? \Carbon\Carbon::parse($change->off_signers)->format('M d, Y h:i A') : '' }}</td>
-                    <td>{{ $change?->joiner_ranks ?? '' }}</td>
-                    <td>{{ $change?->off_signer_ranks ?? '' }}</td>
-                    <td>{{ $change?->total_crew_change ?? '' }}</td>
-                    <td>{{ $change?->reason_change ?? '' }}</td>
-                    <td>{{ $change?->remarks ?? '' }}</td>
-
-                    {{-- Remarks and Master --}}
-                    <td>{{ $report->remarks->remarks ?? '' }}</td>
-                    <td>{{ $report->master_info->master_info ?? '' }}</td>
+                    {{-- General --}}
+                    <td style="width: 150px;">{{ $report->remarks->remarks ?? '' }}</td>
+                    <td style="width: 150px;">{{ $report->master_info->master_info ?? '' }}</td>
                 </tr>
             @endfor
         @endforeach
