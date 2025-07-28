d8b24e0dcb50585bdc2b6c0d70e711eb7bfb2cac
Allen <allenlabrague06@gmail.com>
2025-07-28 23:55:04 +0800

    refactor: Increase column widths for improved readability in weekly schedule reports

--- resources/views/exports/weekly-schedule-reports.blade.php
+++ resources/views/exports/weekly-schedule-reports.blade.php
@@ -7,16 +7,16 @@
     </tr>
 
     <tr>
-        <td style="width: 150px;"><strong>Vessel Name:</strong></td>
-        <td colspan="1" style="width: 150px; text-align: left">{{ $reports->first()->vessel->name ?? '' }}</td>
+        <td style="width: 250px;"><strong>Vessel Name:</strong></td>
+        <td colspan="1" style="width: 250px; text-align: left">{{ $reports->first()->vessel->name ?? '' }}</td>
     </tr>
     <tr>
-        <td style="width: 150px;"><strong>Voyage No:</strong></td>
-        <td colspan="1" style="width: 150px; text-align: left">{{ $reports->first()->voyage_no ?? '' }}</td>
+        <td style="width: 250px;"><strong>Voyage No:</strong></td>
+        <td colspan="1" style="width: 250px; text-align: left">{{ $reports->first()->voyage_no ?? '' }}</td>
     </tr>
     <tr>
-        <td style="width: 150px;"><strong>Date:</strong></td>
-        <td colspan="1" style="width: 150px; text-align: left">
+        <td style="width: 250px;"><strong>Date:</strong></td>
+        <td colspan="1" style="width: 250px; text-align: left">
             {{ optional($reports->first()->all_fast_datetime)
                 ? \Carbon\Carbon::parse($reports->first()->all_fast_datetime)->format('M d, Y h:i A')
                 : '' }}
@@ -32,52 +32,53 @@
         @foreach ($report->ports as $i => $port)
             {{-- Port Info Header --}}
             <tr>
-                <td colspan="6" style="font-weight: bold; width: 150px;">
+                <td colspan="6" style="font-weight: bold; width: 250px;">
                     Port {{ $i + 1 }} - {{ $port->port ?? '' }}
                 </td>
             </tr>
             <tr>
-                <td style="width: 150px; text-align: left"><strong>Activity</strong></td>
-                <td style="width: 150px; text-align: left">{{ $port->activity ?? '' }}</td>
-                <td style="width: 150px; text-align: left"><strong>ETA/ETB</strong></td>
-                <td style="width: 150px; text-align: left">{{ $port->eta_etb ?? '' }}</td>
-                <td style="width: 150px; text-align: left"><strong>ETCD</strong></td>
-                <td style="width: 150px; text-align: left">{{ $port->etcd ?? '' }}</td>
+                <td style="width: 250px; text-align: left"><strong>Activity</strong></td>
+                <td style="width: 250px; text-align: left">{{ $port->activity ?? '' }}</td>
+                <td style="width: 250px; text-align: left"><strong>ETA/ETB</strong></td>
+                <td style="width: 250px; text-align: left">{{ $port->eta_etb ?? '' }}</td>
+                <td style="width: 250px; text-align: left"><strong>ETCD</strong></td>
+                <td style="width: 250px; text-align: left">{{ $port->etcd ?? '' }}</td>
             </tr>
             <tr>
-                <td style="width: 150px; text-align: left"><strong>Cargo</strong></td>
-                <td style="width: 150px; text-align: left">{{ $port->cargo ?? '' }}</td>
-                <td style="width: 150px; text-align: left"><strong>Cargo Qty</strong></td>
-                <td style="width: 150px; text-align: left">{{ $port->cargo_qty ?? '' }}</td>
-                <td style="width: 150px; text-align: left"><strong>Remarks</strong></td>
-                <td style="width: 150px; text-align: left">{{ $port->remarks ?? '' }}</td>
+                <td style="width: 250px; text-align: left"><strong>Cargo</strong></td>
+                <td style="width: 250px; text-align: left">{{ $port->cargo ?? '' }}</td>
+                <td style="width: 250px; text-align: left"><strong>Cargo Qty</strong></td>
+                <td style="width: 250px; text-align: left">{{ $port->cargo_qty ?? '' }}</td>
+                <td style="width: 250px; text-align: left"><strong>Remarks</strong></td>
+                <td style="width: 250px; text-align: left">{{ $port->remarks ?? '' }}</td>
             </tr>
 
             {{-- Agents Header --}}
             <tr>
-                <td colspan="6" style="font-weight: bold; width: 150px;">Agent(s)</td>
+                <td colspan="6" style="font-weight: bold; width: 250px;">Agent(s)</td>
             </tr>
             <tr>
-                <th style="border:1px solid #000; width: 150px; text-align: center;"><strong>Name</strong></th>
-                <th style="border:1px solid #000; width: 150px; text-align: center;"><strong>Address</strong></th>
-                <th style="border:1px solid #000; width: 150px; text-align: center;"><strong>PIC</strong></th>
-                <th style="border:1px solid #000; width: 150px; text-align: center;"><strong>Phone</strong></th>
-                <th style="border:1px solid #000; width: 150px; text-align: center;"><strong>Mobile</strong></th>
-                <th style="border:1px solid #000; width: 150px; text-align: center;"><strong>Email</strong></th>
+                <th style="border:1px solid #000; width: 250px; text-align: center;"><strong>Name</strong></th>
+                <th style="border:1px solid #000; width: 250px; text-align: center;"><strong>Address</strong></th>
+                <th style="border:1px solid #000; width: 250px; text-align: center;"><strong>PIC</strong></th>
+                <th style="border:1px solid #000; width: 250px; text-align: center;"><strong>Phone</strong></th>
+                <th style="border:1px solid #000; width: 250px; text-align: center;"><strong>Mobile</strong></th>
+                <th style="border:1px solid #000; width: 250px; text-align: center;"><strong>Email</strong></th>
             </tr>
 
             @forelse($port->agents as $agent)
                 <tr>
-                    <td style="border:1px solid #000; width: 150px; text-align: left;">{{ $agent->name ?? '' }}</td>
-                    <td style="border:1px solid #000; width: 150px; text-align: left;">{{ $agent->address ?? '' }}</td>
-                    <td style="border:1px solid #000; width: 150px; text-align: left;">{{ $agent->pic_name ?? '' }}</td>
-                    <td style="border:1px solid #000; width: 150px; text-align: left;">{{ $agent->telephone ?? '' }}</td>
-                    <td style="border:1px solid #000; width: 150px; text-align: left;">{{ $agent->mobile ?? '' }}</td>
-                    <td style="border:1px solid #000; width: 150px; text-align: left;">{{ $agent->email ?? '' }}</td>
+                    <td style="border:1px solid #000; width: 250px; text-align: left;">{{ $agent->name ?? '' }}</td>
+                    <td style="border:1px solid #000; width: 250px; text-align: left;">{{ $agent->address ?? '' }}</td>
+                    <td style="border:1px solid #000; width: 250px; text-align: left;">{{ $agent->pic_name ?? '' }}</td>
+                    <td style="border:1px solid #000; width: 250px; text-align: left;">{{ $agent->telephone ?? '' }}</td>
+                    <td style="border:1px solid #000; width: 250px; text-align: left;">{{ $agent->mobile ?? '' }}</td>
+                    <td style="border:1px solid #000; width: 250px; text-align: left;">{{ $agent->email ?? '' }}</td>
+                    <td></td>
                 </tr>
             @empty
                 <tr>
-                    <td colspan="6" style="width: 150px;">No agent data available</td>
+                    <td colspan="6" style="width: 250px;">No agent data available</td>
                 </tr>
             @endforelse
         @endforeach
