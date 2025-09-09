132d42d01549781f8a7c4d16459e96079aba1bca
Allen <allenlabrague06@gmail.com>
2025-09-09 13:28:21 +0800

    feat: Add detailed voyage migration with comprehensive fields for reporting and vessel management

---
+++ database/migrations/2025_06_08_045354_create_voyages_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voyages', function (Blueprint $table) {
+            $table->id();
+            // Constact all report must have
+            $table->foreignId('vessel_id')->constrained()->onDelete('cascade');
+            $table->foreignId('voyage_no')->constrained()->onDelete('users');
+            $table->string('report_type');
+
+            // For All Fast
+            $table->dateTime('all_fast_datetime')->nullable(); // in local time
+            $table->string('port')->nullable();
+            $table->string('gmt_offset')->nullable(); // e.g. "+08:00"
+
+            // For Bunkering
+            $table->string('bunkering_port')->nullable();
+            $table->string('supplier')->nullable();
+            $table->dateTime('port_etd')->nullable();
+            $table->string('port_gmt_offset')->nullable();
+            $table->dateTime('bunker_completed')->nullable();
+            $table->string('bunker_gmt_offset')->nullable();
+
+            $table->string('call_sign')->nullable();
+            $table->string('flag')->nullable();
+            $table->string('port_of_registry')->nullable();
+            $table->string('official_number')->nullable();
+            $table->string('imo_number')->nullable();
+            $table->string('class_society')->nullable();
+            $table->string('class_no')->nullable();
+            $table->string('pi_club')->nullable();
+            $table->string('loa')->nullable();
+            $table->string('lbp')->nullable();
+            $table->string('breadth_extreme')->nullable();
+            $table->string('depth_moulded')->nullable();
+            $table->string('height_maximum')->nullable();
+            $table->string('bridge_front_bow')->nullable();
+            $table->string('bridge_front_stern')->nullable();
+            $table->string('light_ship_displacement')->nullable();
+            $table->dateTime('keel_laid')->nullable();
+            $table->dateTime('launched')->nullable();
+            $table->dateTime('delivered')->nullable();
+            $table->string('shipyard')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voyages');
+    }
+};
